c6cc9ad9731469f41845b66f8a9f65e819a3d0ca
Nikita Schelokov <83090357+Xopek1337@users.noreply.github.com>
2022-12-07 16:45:21 +0300

    move createH to separate file

--- nyquistFilter.m
+++ nyquistFilter.m
@@ -22,24 +22,3 @@ subplot(3,2,[3 4]); stem(data); grid on; axis('tight');
 title('Символы на входе фильтра'); ylabel('амплитуда'); xlabel('символы'); 
 subplot(3,2,[5 6]); plot(y); grid on; axis('tight');  
 title('Символы на выходе фильтра');ylabel('амплитуда'); xlabel('выборки');
-
-function [h] = createH(L, Ts, T, beta) % фильтр Найквиста типа 'Приподнятый косинус'
-    h=zeros(1,L); 
-    if mod(L,2)==0     
-        M=L/2;
-    else
-        M=(L-1)/2;
-    end
-    
-    for n=-M:M     
-        num=sin(pi*n*Ts/T)*cos(beta*pi*n*Ts/T);     
-        den=(pi*n*Ts/T)*(1-(2*beta*n*Ts/T)^2);     
-        h(n+M+1)=num/den;     
-        if (1-(2*beta*n*Ts/T)^2)==0         
-            h(n+M+1)=pi/4*sin(pi*n*Ts/T)/(pi*n*Ts/T);     
-        end
-        if n==0         
-            h(n+M+1)=cos(beta*pi*n*Ts/T)/(1-(2*beta*n*Ts/T)^2); % sinc == 1    
-        end
-    end
-end
